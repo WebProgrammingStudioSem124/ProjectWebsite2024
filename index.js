120f3ce19825e8764bcf0306c40b04a8b6ec7fb6
Nguyen Name <152080640+rmitaccs40@users.noreply.github.com>
2024-04-20 19:34:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
+// Import necessary modules
 const express = require('express');
+
+// Initialize Express application
 const app = express();
 const port = 9000;
+
+
+// Import routers
+
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 
+
+// Get the webpage
 app.get('/', (req, res) => {
   // Redirect to the homepage
   res.redirect('/homepage');
 });
-
-app.get('/homepage', (req, res) => {
-  // Render the 'homepage' view
-  res.render('homepage');
-});
-
 app.get('/login', (req, res) => {
   // Render the 'login' view
   res.render('login');
 });
 
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
+
+// Use middleware
 // Serve the static js and css files in public folder
 app.use(express.static('public'));
