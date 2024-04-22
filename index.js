d671bf3f2ab6be16237b53508b698860e3b7e614
NgPhUyen <s4037158@rmit.edu.vn>
2024-04-22 11:50:46 +0700

    Build navbar and add to homepage.ejs

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ app.set('view engine', 'ejs');
 
 // Middleware
 // logs the request method and path to the console. 
-function logquest(req, res, next){
-  console.log(`Request: ${req.method} for ${req.path}`);
-  next();
-}
+// function logquest(req, res, next){
+//   console.log(`Request: ${req.method} for ${req.path}`);
+//   next();
+// }
 
 // Use middleware
-app.use(logquest);
+// app.use(logquest);
 app.use(express.static('public')); // Serve the static js and css files in public folder
 
 // Routes
@@ -28,11 +28,7 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
   // Render the 'login' view
   res.render('login', {text: '2024'});
-});
-
-app.get('/hello', function(req, res){
-  res.send("<h1>Hello,</h1>");
-});
+})
 
 // Start the server
 app.listen(port, () => {
